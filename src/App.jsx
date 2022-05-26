49bdd51693d3887311e90d3c62d02604be0fb853
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2022-05-26 11:38:30 -0500

    from sandbox

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ const Search = styled("div")(({ theme }) => ({
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
   },
   margin: "20px 0px",
   width: "100%",
   [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: "auto",
-  },
+    width: "auto"
+  }
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
@@ -30,7 +30,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
   pointerEvents: "none",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "center"
 }));
 
 const StyledInputBase = styled(Input)(({ theme }) => ({
@@ -45,10 +45,10 @@ const StyledInputBase = styled(Input)(({ theme }) => ({
     [theme.breakpoints.up("sm")]: {
       width: "60ch",
       "&:focus": {
-        width: "40ch",
-      },
-    },
-  },
+        width: "40ch"
+      }
+    }
+  }
 }));
 
 function App() {
@@ -57,7 +57,7 @@ function App() {
   const topics = [
     {
       id: 0,
-      title: "React",
+      title: "xxx",
       color: "#019890",
       commands: [
         "npx create-create-react app",
@@ -65,8 +65,8 @@ function App() {
         "npm install",
         "npm start",
         "npm run build",
-        "npm run test",
-      ],
+        "npm run test"
+      ]
     },
     {
       id: 1,
@@ -76,20 +76,20 @@ function App() {
         "npx degit sveltejs/template my-svelte-project",
         "cd my-svelte-project",
         "npm install",
-        "npm run dev",
-      ],
+        "npm run dev"
+      ]
     },
     {
       id: 2,
       title: "Vue",
       color: "#00CC65",
-      commands: ["npm init vue@latest"],
+      commands: ["npm init vue@latest"]
     },
     {
       id: 3,
       title: "Angular",
       color: "#F21200",
-      commands: ["npm i @angular/cli", "npm i @angular/material"],
+      commands: ["npm i @angular/cli", "npm i @angular/material"]
     },
     {
       id: 4,
@@ -103,8 +103,8 @@ function App() {
         "git push <nombre-remoto> <nombre-rama>",
         "git branch -d <nombre-de-la-rama>",
         "git checkout <nombre-de-la-rama>",
-        "git commit -m 'mensaje de confirmación'",
-      ],
+        "git commit -m 'mensaje de confirmación'"
+      ]
     },
     {
       id: 5,
@@ -112,8 +112,8 @@ function App() {
       color: "#5A95F3",
       commands: [
         "npm install @mui/material @emotion/react @emotion/styled",
-        "npm install @mui/icons-material",
-      ],
+        "npm install @mui/icons-material"
+      ]
     },
     {
       id: 6,
@@ -124,8 +124,8 @@ function App() {
         "mkdir -> para crear un nuevo directorio",
         "touch -> te permite crear un nuevo archivo en blanco",
         "tail -> mostrará las últimas diez líneas de un archivo de texto",
-        "ping -> para verificar tu estado de conectividad a un servidor",
-      ],
+        "ping -> para verificar tu estado de conectividad a un servidor"
+      ]
     },
     {
       id: 7,
@@ -137,9 +137,9 @@ function App() {
         "npm install",
         "npm run dev",
         "npm run build",
-        "npm run preview",
-      ],
-    },
+        "npm run preview"
+      ]
+    }
   ];
 
   const topicsToShow = topics.filter((topic) =>
@@ -161,7 +161,7 @@ function App() {
               fontWeight: "700",
               display: "inline",
               color: "white",
-              textAlign: "center",
+              textAlign: "center"
             }}
           >
             {">_Lista de Comandos"}
